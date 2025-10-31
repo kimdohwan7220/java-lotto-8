30848aae5b228a0bbb6634da77556530c3b4c82f
kimdohwan7220 <dohwan8220@naver.com>
2025-10-31 22:48:31 +0900

    feat: 로또 번호 생성 시 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +12,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateLottoNumberSize(numbers);
+        validateDuplicateNumber(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateLottoNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        boolean invalid = numbers.stream()
+                .anyMatch(n -> n < 1 || n > 45);
+        if (invalid) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
 }
