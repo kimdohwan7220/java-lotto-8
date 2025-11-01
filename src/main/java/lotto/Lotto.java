e7748e4e8842c4c49214498e335c4ecac6bdb279
kimdohwan7220 <dohwan8220@naver.com>
2025-11-01 11:32:07 +0900

    feat: 랜덤 로또 번호 생성 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,14 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public int calculateLottoCount(int purchasePrice) {
+        return purchasePrice / LOTTO_PRICE;
+    }
+
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_NUMBER,
+                LOTTO_MAX_NUMBER,
+                LOTTO_NUMBER_COUNT);
     }
 
     private void validate(List<Integer> numbers) {
