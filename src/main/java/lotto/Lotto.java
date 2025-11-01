b0e78b8f9e18138ca8cf15c56299f2a3d12b093a
kimdohwan7220 <dohwan8220@naver.com>
2025-11-01 14:18:35 +0900

    refactor: 로또 검증 메서드 세분화

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,11 +1,14 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import utils.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
+
     private static final int LOTTO_PRICE = 1000;
     private static final int LOTTO_MIN_NUMBER = 1;
     private static final int LOTTO_MAX_NUMBER = 45;
@@ -16,15 +19,25 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public int calculateLottoCount(int purchasePrice) {
+    public static int calculateLottoCount(int purchasePrice) {
         return purchasePrice / LOTTO_PRICE;
     }
 
-    public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(
+    public static Lotto generateRandomNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 LOTTO_MIN_NUMBER,
                 LOTTO_MAX_NUMBER,
-                LOTTO_NUMBER_COUNT);
+                LOTTO_NUMBER_COUNT
+        );
+        return new Lotto(numbers);
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(generateRandomNumber());
+        }
+        return lottos;
     }
 
     private void validate(List<Integer> numbers) {
@@ -34,22 +47,29 @@ public class Lotto {
     }
 
     private void validateLottoNumberSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_SIZE.getMessage());
         }
     }
 
     private void validateDuplicateNumber(List<Integer> numbers) {
-        if (numbers.size() != new HashSet<>(numbers).size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
         }
     }
 
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.size() != new HashSet<>(numbers).size();
+    }
+
     private void validateNumberRange(List<Integer> numbers) {
-        boolean invalid = numbers.stream()
-                .anyMatch(n -> n < 1 || n > 45);
-        if (invalid) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이여야 합니다.");
+        if (numbersOutOfRange(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
         }
+    }
+
+    private boolean numbersOutOfRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(n -> n < LOTTO_MIN_NUMBER || n > LOTTO_MAX_NUMBER);
     }
 }
