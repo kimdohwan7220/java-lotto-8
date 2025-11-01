4f89e718a9c7dd852ec97a92d430a3dc243d64d9
kimdohwan7220 <dohwan8220@naver.com>
2025-11-01 12:26:30 +0900

    feat: 에러 메시지 관리용 enum 추가

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -1,4 +1,18 @@
 package utils;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+    INVALID_LOTTO_NUMBER("[ERROR] 로또 번호는 1부터 45 사이여야 합니다."),
+    DUPLICATE_NUMBER("[ERROR] 로또 번호에 중복된 숫자가 있습니다."),
+    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
