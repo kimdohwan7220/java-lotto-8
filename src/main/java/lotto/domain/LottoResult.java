5f07312917b26279234f95efe6fa036e6723045b
kimdohwan7220 <dohwan8220@naver.com>
2025-11-02 16:56:31 +0900

    feat: 등수별 당첨 개수에 관련된 메서드 추가

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,4 +1,27 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LottoResult {
+    private final Map<LottoRank, Integer> rankCounts = new HashMap<>();
+    private final List<Lotto> purchasedLottos;
+
+    public LottoResult(List<Lotto> purchasedLottos) {
+        this.purchasedLottos = purchasedLottos;
+        initRankCounts();
+    }
+
+    private void calculateResults() {
+        for(Lotto lotto : purchasedLottos) {
+
+        }
+    }
+
+    private void initRankCounts() {
+        for(LottoRank rank : LottoRank.values()) {
+            rankCounts.put(rank, 0);
+        }
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,4 @@
 package lotto.view;
 
-public class Outputview {
+public class OutputView {
 }
