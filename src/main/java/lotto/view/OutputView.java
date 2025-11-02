ee70566e4c6f2afda514914cbc29bf61fe738e58
kimdohwan7220 <dohwan8220@naver.com>
2025-11-02 18:21:39 +0900

    feat: 로또 구매 갯수와 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,24 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.utils.ViewMessage;
+
 public class OutputView {
+
+    public static void printPurcahsedLottos(List<Lotto> purchasedLottos) {
+        System.out.println(
+                String.format(
+                        ViewMessage.PURCHASE_COUNT_OUTPUT.getMessage(),
+                        purchasedLottos.size()
+                )
+        );
+
+        for (Lotto lotto : purchasedLottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+
+
 }
