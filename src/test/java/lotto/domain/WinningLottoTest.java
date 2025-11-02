70057fce234b0fefb84d3fadbd4834525e48d460
kimdohwan7220 <dohwan8220@naver.com>
2025-11-02 19:27:15 +0900

    test: WinningLotto 보너스 번호 예외 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,4 +1,31 @@
 package lotto.domain;
 
-public class WinningLottoTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import lotto.utils.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+
+    @DisplayName("보너스 번호가 당첨 번호에 포함되면 예외가 발생한다.")
+    @Test
+    void 보너스번호_중복_예외발생() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 3;
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+    }
+
+    @DisplayName("보너스 번호가 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 보너스번호_범위_벗어나면_예외발생() {
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, 46))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+    }
 }
