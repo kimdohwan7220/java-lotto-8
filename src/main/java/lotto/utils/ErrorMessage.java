f51945688cc818886cdd220d2e6d7639c52a3b7d
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 20:52:36 +0900

    refactor: 에러 메시지 enum 수정

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -5,6 +5,8 @@ public enum ErrorMessage {
     DUPLICATE_NUMBER("[ERROR] 로또 번호에 중복된 숫자가 있습니다."),
     INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
     NOT_ENOUGH_MONEY("[ERROR] 구입 금액이 1000원보다 적습니다."),
+    INPUT_EMPTY("[ERROR] 입력 값이 비어있습니다."),
+    INVALID_NUMBER_FORMAT("[ERROR] 숫자 형식이 올바르지 않습니다."),
     INVALID_PURCHASE_AMOUNT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
 
     private final String message;
