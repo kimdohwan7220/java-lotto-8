2f6c26704d6e8c1f8a265f8879fedccebc1e1ade
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 20:09:49 +0900

    refactor: OutputView 당첨 결과, 수익률 출력 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,24 +1,65 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
+import lotto.utils.LottoFormatter;
 import lotto.utils.ViewMessage;
 
 public class OutputView {
 
-    public static void printPurcahsedLottos(List<Lotto> purchasedLottos) {
-        System.out.println(
-                String.format(
-                        ViewMessage.PURCHASE_COUNT_OUTPUT.getMessage(),
-                        purchasedLottos.size()
-                )
-        );
+    public static void printPurchasedLottos(List<Lotto> purchasedLottos) {
+        printPurchaseCount(purchasedLottos.size());
+        printLottoNumbers(purchasedLottos);
+    }
 
+    private static void printPurchaseCount(int count) {
+        System.out.println(String.format(
+                ViewMessage.PURCHASE_COUNT_OUTPUT.getMessage(), count));
+    }
+
+    private static void printLottoNumbers(List<Lotto> purchasedLottos) {
         for (Lotto lotto : purchasedLottos) {
             System.out.println(lotto.getNumbers());
         }
     }
 
+    public static void printStatistics(LottoResult lottoResult) {
+        printResultHeader();
+        printRankStatistics(lottoResult.getRankCounts());
+        printProfitRate(lottoResult.calculateProfitRate());
+    }
 
+    private static void printResultHeader() {
+        System.out.println(ViewMessage.RESULT_OUTPUT.getMessage());
+        System.out.println(ViewMessage.RESULT_SEPARATOR.getMessage());
+    }
+
+    private static void printRankStatistics(Map<LottoRank, Integer> rankCounts) {
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank == LottoRank.NONE) continue;
+            System.out.println(formatRankStatus(rank, rankCounts.get(rank)));
+        }
+    }
+
+    private static String formatRankStatus(LottoRank rank, int count) {
+        String matchText = String.format(ViewMessage.MATCH_COUNT_OUTPUT.getMessage(), rank.getMatchCount());
+        if (rank == LottoRank.SECOND) {
+            matchText += ViewMessage.BONUS_MATCH.getMessage();
+        }
+
+        String prizeCountText = String.format(ViewMessage.PRIZE_COUNT_OUTPUT.getMessage(),
+                String.format("%,d", rank.getPrize()),
+                count);
+
+        return matchText + prizeCountText;
+    }
+
+    private static void printProfitRate(double profitRate) {
+        String formattedRate = LottoFormatter.PROFIT_FORMAT.format(profitRate);
+        System.out.printf(ViewMessage.PROFITRATE_OUTPUT.getMessage() + "%n", formattedRate);
+    }
 
 }
