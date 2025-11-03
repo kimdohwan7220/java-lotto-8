49a50d32870b3e046c6aceb37de0ab764fc1989c
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 20:55:42 +0900

    feat: 컨트롤러 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,27 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoPurchase;
+import lotto.domain.LottoResult;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.utils.WinningLottoParser;
+
 public class LottoController {
+
+    public void run() {
+
+        int amount = Integer.parseInt(InputView.inputPurchaseAmount());
+        List<Lotto> purchased = LottoPurchase.buyLottos(amount);
+        OutputView.printPurchasedLottos(purchased);
+
+        List<Integer> numbers = WinningLottoParser.parseWinningNumbers(InputView.inputWinningNumbers());
+        int bonus = Integer.parseInt(InputView.inputBonusNumber());
+        WinningLotto winningLotto = new WinningLotto(numbers, bonus);
+
+        LottoResult result = new LottoResult(purchased, winningLotto);
+        OutputView.printStatistics(result);
+    }
 }
