a753ead04fb76bd45cd676f5efcdd20a924ea5ef
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 20:55:05 +0900

    feat: 당첨 번호 쉼표 기준으로 분리 기능 추가

--- src/main/java/lotto/utils/WinningLottoParser.java
+++ src/main/java/lotto/utils/WinningLottoParser.java
@@ -1,4 +1,28 @@
 package lotto.utils;
 
+import lotto.utils.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WinningLottoParser {
+    public static List<Integer> parseWinningNumbers(String input) {
+        validateNotBlank(input);
+
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+        }
+    }
+
+    private static void validateNotBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_EMPTY.getMessage());
+        }
+    }
 }
