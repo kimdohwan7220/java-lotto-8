f619e8601be1bf0896ec698bd7fdddb2a75c3c72
kimdohwan7220 <dohwan8220@naver.com>
2025-11-03 20:09:15 +0900

    refactor: ViewMessage 메시지 추가

--- src/main/java/lotto/utils/ViewMessage.java
+++ src/main/java/lotto/utils/ViewMessage.java
@@ -5,8 +5,12 @@ public enum ViewMessage {
     WINNING_NUMBER_INPUT("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요."),
     RESULT_OUTPUT("당첨 통계"),
+    RESULT_SEPARATOR("---"),
     PURCHASE_COUNT_OUTPUT("%d개를 구매했습니다."),
-    PROFITRATE_OUTPUT("총 수익률은 %s%%입니다.");
+    PROFITRATE_OUTPUT("총 수익률은 %s%%입니다."),
+    BONUS_MATCH(", 보너스 볼 일치"),
+    MATCH_COUNT_OUTPUT("%d개 일치"),
+    PRIZE_COUNT_OUTPUT(" (%s원) - %d개");
 
 
     private final String message;
